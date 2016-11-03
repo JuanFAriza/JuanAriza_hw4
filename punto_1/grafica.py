0f967c5942a102a142cf9a97640a6a983005afea
Juan Felipe Ariza Mejia <jf.ariza10@uniandes.edu.co>
2016-11-02 21:25:12 -0500

    Carga los datos de potencial, campo eléctrico Ex y Ey como numpy arrays

--- punto_1/grafica.py
+++ punto_1/grafica.py
@@ -1,1 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+file = open('datos.dat', 'rt') # Abre el archivo de datos
+
+try:
+    lector = csv.reader(file) # Lee el archivo de datos
+    datos = []
+    for linea in lector: # Agrega cada linea a la lista de datos
+        datos.append(linea)
+
+finally:
+    file.close()
+
+medidas = []
+for dato in datos:
+    medida = dato[0]
+    a = medida.split(' ') # Separa por espacios los valores de cada linea
+    del a[-1]
+    medidas.append(a)
+
+V = np.array(medidas[0:250],dtype='float')
+Ex = np.array(medidas[250:500],dtype='float')
+Ey = np.array(medidas[500:],dtype='float')
+
 archivo = 'placas.pdf'
